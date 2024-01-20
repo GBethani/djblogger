7078d5ef7b5c6a0447c9a152d34898d856fa31f6
GBethani <jinkiller69@gmail.com>
2024-01-20 09:25:47 +0530

    blog models added

--- djblogger/blog/models.py
+++ djblogger/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Article(models.Model):
+    options = (
+        ('draft','Draft'),
+        ('published','Published')
+    )
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(to=User,on_delete=models.CASCADE, related_name='Articles')
+    status = models.CharField(max_length=15,choices=options,default='draft')
+    date_created = models.DateTimeField(auto_now_add=True,editable=False)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-date_created",)
+
+    def __str__(self):
+        return self.title
