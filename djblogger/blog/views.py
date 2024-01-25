7bfb3df3f896301262419f2b9bb70cda7ce460e2
GBethani <jinkiller69@gmail.com>
2024-01-25 12:26:44 +0530

    views and templates added

--- djblogger/blog/views.py
+++ djblogger/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from . import models
 # Create your views here.
+class HomeView(ListView):
+    model = models.Article
+    template_name = 'blog/index.html'
