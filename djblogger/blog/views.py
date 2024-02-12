e09b0020668e1f021fef991c6bcdca8701ee2b6e
GBethani <jinkiller69@gmail.com>
2024-02-12 10:35:33 +0530

    pagination added

--- djblogger/blog/views.py
+++ djblogger/blog/views.py
@@ -6,3 +6,7 @@ class HomeView(ListView):
     model = models.Article
     template_name = 'blog/index.html'
     context_object_name = 'articles'
+    paginate_by = 10  # Set the number of articles per page
+
+    def get_queryset(self):
+        return models.Article.objects.all().order_by('-date_created')
